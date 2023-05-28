71449b2dfadc5ee27dea269035cdcf903c9b1f66
feerdaus <firdauslawal@gmail.com>
2023-05-28 22:19:51 +0100

    Add about

--- src/lib/images/landing-page.ts
+++ src/lib/images/landing-page.ts
@@ -1,5 +1,11 @@
 export const HERO_IMAGE = {
-  src: '/hero_img.png',
+  src: "/hero_img.png",
   height: 1518,
-  width: 1518
-}
+  width: 1518,
+};
+
+export const ABOUT_IMAGE = {
+  src: "/about_img.png",
+  height: 1261,
+  width: 1368,
+};
